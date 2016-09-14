ab102a8127f68e69dfc33b921e89dcbe1afaad01
thaspdev <thaspdev@users.noreply.github.com>
2016-09-14 15:13:56 +0200

    Create algorithm.py

---
+++ algorithm.py
@@ -0,0 +1,11 @@
+for i in range (2000,5000,5) :
+   termStart = i + 2
+   termEnd = termStart + 5
+   OGDatesArray = []
+   for j in range (termStart, termEnd,1) :
+       if j%4==0 :
+           OGDatesArray.append(j)
+   if len(OGDatesArray) >= 2 :
+       print("Term ", termStart, "-", termEnd, " : ")
+       for x in arrayOGDates :
+           print(x)
