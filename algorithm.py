eb796c1f80685a6d2bfc4341c4312af5064477b1
thaspdev <thaspdev@users.noreply.github.com>
2016-09-14 15:14:54 +0200

    Added missing space

--- algorithm.py
+++ algorithm.py
@@ -2,7 +2,7 @@ for i in range (2000,5000,5) :
    termStart = i + 2
    termEnd = termStart + 5
    OGDatesArray = []
-   for j in range (termStart, termEnd,1) :
+   for j in range (termStart, termEnd, 1) :
        if j%4==0 :
            OGDatesArray.append(j)
    if len(OGDatesArray) >= 2 :
